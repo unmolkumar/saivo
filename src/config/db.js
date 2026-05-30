0ef6fdb21247dca20e5d96e4de7d62d27f7827a9
unmolkumar <anmolkumar.login@gmail.com>
2026-05-31 02:22:56 +0530

    connect to database

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to database");
+  } catch (err) {
+    console.log("database connection failed : " + err);
+  }
+}
+
+module.exports = connectDB;
