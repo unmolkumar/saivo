f3c6a154aa611c332c350723669043a7d1bf8280
unmolkumar <anmolkumar.login@gmail.com>
2026-05-31 07:13:26 +0530

    test : add login api test

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -43,4 +43,88 @@ describe("auth routes", () => {
       );
     });
   });
+
+  describe("POST /login", () => {
+    test("should login with correct credentials", async () => {
+      await request(app).post("/api/auth/register").send({
+        username: "unmolkumar",
+        password: "1234567890",
+        email: "unmolkumar.login@gmail.com",
+      });
+      const response = await request(app).post("/api/auth/login").send({
+        username: "unmolkumar",
+        password: "1234567890",
+      });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toBe("login successfull");
+    });
+
+    test("should not login with incorrect credentials", async () => {
+      await request(app).post("/api/auth/register").send({
+        username: "unmolkumar",
+        password: "1234567890",
+        email: "unmolkumar.login@gmail.com",
+      });
+      const response = await request(app).post("/api/auth/login").send({
+        username: "unmolkumar",
+        password: "4823748234",
+      });
+      expect(response.statusCode).toBe(401);
+      expect(response.body.message).toBe("invalid credentials");
+    });
+
+    test("should detect invalid user", async () => {
+      await request(app).post("/api/auth/register").send({
+        username: "unmolkumar",
+        password: "1234567890",
+        email: "unmolkumar.login@gmail.com",
+      });
+      const response = await request(app).post("/api/auth/login").send({
+        username: "anmolkumar",
+        password: "1234567890",
+      });
+      expect(response.statusCode).toBe(401);
+      expect(response.body.message).toBe("user does not exist");
+    });
+
+    test("should login with correct token", async () => {
+      await request(app).post("/api/auth/register").send({
+        username: "unmolkumar",
+        password: "1234567890",
+        email: "unmolkumar.login@gmail.com",
+      });
+      const loginResponse = await request(app).post("/api/auth/login").send({
+        username: "unmolkumar",
+        password: "1234567890",
+      });
+      const cookie = loginResponse.headers["set-cookie"];
+      const response = await request(app)
+        .post("/api/auth/login")
+        .set("Cookie", cookie)
+        .send({
+          username: "unmolkumar",
+          password: "1234567890",
+        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toBe("login successfull");
+    });
+
+    test("should not login with incorrect token", async () => {
+      await request(app).post("/api/auth/register").send({
+        username: "unmolkumar",
+        password: "1234567890",
+        email: "unmolkumar.login@gmail.com",
+      });
+      const cookie = "token=fakeassfuckcookie";
+      const response = await request(app)
+        .post("/api/auth/login")
+        .set("Cookie", cookie)
+        .send({
+          username: "unmolkumar",
+          password: "1234567890",
+        });
+      expect(response.statusCode).toBe(401);
+      expect(response.body.message).toBe("invalid token");
+    });
+  });
 });
